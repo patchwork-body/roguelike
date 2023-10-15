8deadb264a42d59ede76e74963760b314e0571a3
patchwork-body <personal.gugfug@gmail.com>
2023-10-15 08:46:28 +0200

    feat: camera movement

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,9 @@ const flowerNeighbor = ['!']
 const rockNeighbor = ['*', '-', '!']
 const rockCrackedNeighbors = ['*', '!']
 
+const WIDTH_TILE_COUNT = 100;
+const HEIGHT_TILE_COUNT = 100;
+
 const getRandomTile = (tiles: string[]): TileSymbol => {
   const tilesI = Math.max(Math.round((Math.random() * tiles.length - 1)), 0)
   return tiles[tilesI] as TileSymbol
@@ -53,9 +56,9 @@ const getNextTile = (tile?: TileSymbol) => {
 const generateMap = () => {
   const map = []
 
-  for (let y = 0; y <= 100; y++) {
+  for (let y = 0; y <= WIDTH_TILE_COUNT; y++) {
     map[y] = [getNextTile()]
-    for (let x = 0; x <= 100; x++) {
+    for (let x = 0; x <= HEIGHT_TILE_COUNT; x++) {
       const nextTile = getNextTile(map[y][x])
       map[y].push(nextTile)
     }
@@ -71,6 +74,23 @@ const FPS = 60;
 const mapTiles = new Image();
 mapTiles.src = '/assets/map-tiles.png';
 
+const camera = [0, 0]
+let keyPressed: 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight' | undefined = undefined; 
+
+document.addEventListener('keydown', (event) => {
+  if(['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+    event.preventDefault();
+    keyPressed = event.key;
+  }
+})
+
+document.addEventListener('keyup', (event) => {
+  if(['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+    event.preventDefault();
+    keyPressed = undefined;
+  }
+})
+
 const animate: FrameRequestCallback = (timestamp) => {
   const deltaTime = timestamp - lastTime;
   const interval = 1000 / FPS;
@@ -79,9 +99,19 @@ const animate: FrameRequestCallback = (timestamp) => {
     let x = 0
     context?.clearRect(0, 0, app.width, app.height);
 
+    if (keyPressed === 'ArrowUp') {
+      camera[1] = Math.min(camera[1] + 10, 0)
+    } else if (keyPressed === 'ArrowDown') {
+      camera[1] = Math.max(camera[1] - 10, -HEIGHT_TILE_COUNT * 32 + app.height);
+    } else if (keyPressed === 'ArrowLeft') {
+      camera[0] = Math.min(camera[0] + 10, 0)
+    } else if (keyPressed === 'ArrowRight') {
+      camera[0] = Math.max(camera[0] - 10, -WIDTH_TILE_COUNT * 32 + app.width)
+    }
+
     GAME_MAP.forEach((line, yIndex) => {
       line.forEach((symbol, xIndex) => {
-        context?.drawImage(mapTiles, ...TILES[symbol], 16, 16, xIndex * 32, yIndex * 32, 32, 32)
+        context?.drawImage(mapTiles, ...TILES[symbol], 16, 16, camera[0] + xIndex * 32, camera[1] + yIndex * 32, 32, 32)
       })
     })
   }
